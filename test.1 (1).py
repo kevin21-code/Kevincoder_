a8cd23f7fc4195153c995c5cc6dac9b42bc2d6bf
kevin21-code <kevin.2024.del@gmail.com>
2024-12-08 23:51:33 -0300

    Add files via upload

---
+++ test.1 (1).py
@@ -0,0 +1,13 @@
+from mi_primer_paquete import Pre_Entrega2
+
+
+cliente = Pre_Entrega2.Cliente (12345678-2,"Kevin", " Mendez",21,"kevin.2024.del@gmail.com","Tarjeta de debito")
+
+
+name = input("Ingrese el nombre del cliente: ")
+lastName = input("Ingrese el apellido del cliente: ")
+age = int(input("Ingrese la edad del cliente: "))
+mail = input("Ingrese el email del cliente: ")
+payment_method = input("Ingrese el método de pago del cliente: ")
+
+   
